5e38372e5d0bd9643a8fbbeab19c2294c42f9b60
Madhur82954 <itzmadhurverma@gmail.com>
2023-05-25 12:27:02 +0530

    [Madhur] - Playing 2 Players Added

--- Program.cs
+++ Program.cs
@@ -7,51 +7,58 @@ namespace SnakeAndLadderProblem
         static void Main(string[] args)
         {
             //UC1-start and end Position
-            int currpos = 0;
             int Finalpos = 100;
-            Console.WriteLine("Start pos = " + currpos);
+            Console.WriteLine("Start pos = 0" );
+            int[] playerposition = new int[2];
+            int currentplayer = 0;
             int DiceRoll = 0;
 
             //UC2- RollDie
             Random random = new Random();
 
-            while (currpos < Finalpos)
+            while (playerposition[currentplayer] < Finalpos)
             {
 
                 int Dieroll=random.Next(1, 6);
-                Console.WriteLine("Player roll a die get = " + Dieroll);
+                Console.WriteLine("Player" + (currentplayer + 1)  + "roll a die get = " + Dieroll);
 
                 //UC3-Check For Option
                 int option = random.Next(0, 3);
                     switch (option)
                     {
-                        case 0:
+                        case 0://No play
                             Console.WriteLine("No Play. The Player Stays in Same Position");
                             break;
 
-                        case 1:
-                            Console.WriteLine("Ladder. The Player Moves Ahead By " + Dieroll + " Positions");
+                        case 1: // Ladder
+                            Console.WriteLine("Ladder. The Player" + (currentplayer + 1) + "Moves Ahead By " + Dieroll + " Positions");
 
-                            if(currpos + Dieroll <= Finalpos)
-                                currpos += Dieroll;
+                            if(playerposition[currentplayer] + Dieroll <= Finalpos)
+                                playerposition[currentplayer] += Dieroll;
                             break;
 
-                        case 2:
-                            Console.WriteLine("Snake . The Player Moves Behind By " + Dieroll + " Positions");
-                            currpos -= Dieroll;
+                        case 2: //Snake
+                            Console.WriteLine("Snake . The Player" + (currentplayer + 1) + "Moves Behind By " + Dieroll + " Positions");
+                            playerposition[currentplayer] -= Dieroll;
 
-                            if (currpos < 0)
+                            if (playerposition[currentplayer] < 0)
                             {
-                                currpos = 0;
+                                playerposition[currentplayer] = 0; //Restart from 0 if position below 0
                             }
                             break;
                     }
                     DiceRoll++;
-                    Console.WriteLine("Current Position = " + currpos);
+                    Console.WriteLine("Player" + (currentplayer + 1) +"Current Position = " + playerposition[currentplayer]);
+
+                if (option != 1)
+                {
+                    currentplayer = (currentplayer + 1) % 2;
+                }
 
             }
             Console.WriteLine("Congratulations! Player Has reached to the winning position");
             Console.WriteLine("Number of DiceRolls = " + DiceRoll);
+            Console.WriteLine("Player" + (currentplayer + 1) + "Wins the game");
         }
     }
 }
