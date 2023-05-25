76ab5c6a4fdc8faa8434d638b77d601fa2c20a86
Madhur82954 <itzmadhurverma@gmail.com>
2023-05-25 11:35:56 +0530

    [Madhur]- Roll Die Added

--- Program.cs
+++ Program.cs
@@ -11,6 +11,10 @@ namespace SnakeAndLadderProblem
             int Finalpos = 100;
             Console.WriteLine("Start pos = " + currpos);
 
+            //UC2- RollDie
+            Random random = new Random();
+            int Dieroll=random.Next(1, 6);
+            Console.WriteLine("Player roll a die get = " + Dieroll);
         }
     }
 }
