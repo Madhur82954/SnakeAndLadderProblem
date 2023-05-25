a5db5764839cf013f748bbc33b4ee20003e142be
Madhur82954 <itzmadhurverma@gmail.com>
2023-05-25 11:31:36 +0530

    [Madhur] - Start Position Added

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SnakeAndLadderProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1-start and end Position
+            int currpos = 0;
+            int Finalpos = 100;
+            Console.WriteLine("Start pos = " + currpos);
+
+        }
+    }
+}
